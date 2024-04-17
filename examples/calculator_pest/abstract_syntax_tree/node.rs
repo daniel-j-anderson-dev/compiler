3cc91946f968331568c557cacdad0f296257f42b
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-04-16 21:15:55 -0400

    fewer impls of try_from

--- examples/calculator_pest/abstract_syntax_tree/node.rs
+++ examples/calculator_pest/abstract_syntax_tree/node.rs
@@ -20,31 +20,20 @@ pub enum Node {
         rhs: Box<Node>,
     },
 }
-impl Display for Node {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        return match self {
-            Node::Integer(value) => write!(f, "{}", value),
-            Node::UnaryExpression { operator, operand } => write!(f, "{}{}", operator, operand),
-            Node::BinaryExpression { operator, lhs, rhs } => {
-                write!(f, "{} {} {}", lhs, operator, rhs)
-            }
-        };
-    }
-}
-impl TryFrom<(Pair<'_>, Node)> for Node {
-    type Error = ParseNodeError;
-    fn try_from((pair, operand): (Pair<'_>, Node)) -> Result<Self, Self::Error> {
+impl Node {
+    pub fn new_unary_expression(operator: Pair<'_>, operand: Node) -> Result<Self, ParseNodeError> {
         return Ok(Node::UnaryExpression {
-            operator: pair.as_str().parse()?,
+            operator: operator.as_str().parse()?,
             operand: Box::new(operand),
         });
     }
-}
-impl TryFrom<(Pair<'_>, Node, Node)> for Node {
-    type Error = ParseNodeError;
-    fn try_from((pair, lhs, rhs): (Pair<'_>, Node, Node)) -> Result<Self, Self::Error> {
+    pub fn new_binary_expression(
+        operator: Pair<'_>,
+        lhs: Node,
+        rhs: Node,
+    ) -> Result<Self, ParseNodeError> {
         return Ok(Node::BinaryExpression {
-            operator: pair.as_str().parse()?,
+            operator: operator.as_str().parse()?,
             lhs: Box::new(lhs),
             rhs: Box::new(rhs),
         });
@@ -70,19 +59,19 @@ impl TryFrom<Pair<'_>> for Node {
             Rule::UnaryExpression => {
                 let operator = pair.next().ok_or(ParseNodeError::NoNextPair)?;
                 let operand = Node::try_from(pair.next())?;
-                Node::try_from((operator, operand))
+                Node::new_unary_expression(operator, operand)
             }
             Rule::BinaryExpression => {
                 let mut lhs = Node::try_from(pair.next())?;
                 let op = pair.next().ok_or(ParseNodeError::NoNextPair)?;
                 let mut rhs = Node::try_from(pair.next())?;
 
-                let mut ret_val = Node::try_from((op, lhs, rhs))?;
+                let mut ret_val = Node::new_binary_expression(op, lhs, rhs)?;
                 loop {
                     if let Some(operator) = pair.next() {
                         lhs = ret_val;
                         rhs = pair.next().try_into()?;
-                        ret_val = Node::try_from((operator, lhs, rhs))?;
+                        ret_val = Node::new_binary_expression(operator, lhs, rhs)?;
                     } else {
                         return Ok(ret_val);
                     }
@@ -99,6 +88,17 @@ impl TryFrom<Pair<'_>> for Node {
                 Ok(Node::Integer(sign * value))
             }
             unknown => Err(ParseNodeError::UnrecognizedRule(unknown)),
+        };
+    }
+}
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return match self {
+            Node::Integer(value) => write!(f, "{}", value),
+            Node::UnaryExpression { operator, operand } => write!(f, "{}{}", operator, operand),
+            Node::BinaryExpression { operator, lhs, rhs } => {
+                write!(f, "{} {} {}", lhs, operator, rhs)
+            }
         };
     }
 }
